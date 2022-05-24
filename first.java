23398856223b48aa67041c887cb619e31627c7ef
Manjot08 <Manjotsingh15899@Gmail.com>
2022-05-24 23:29:28 +0530

    First Program in JAVA

---
+++ first.java
@@ -0,0 +1,10 @@
+public class first {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World!!"); // prints Hello World
+   }
+}
