53dad3149685dd0e0661d1f306d732b671013fd5
Manjot08 <59700302+Manjothub@users.noreply.github.com>
2022-05-25 23:48:11 +0530

    Add files via upload

---
+++ Basic_Programs/src/Programs/Main.java
@@ -0,0 +1,15 @@
+package Programs; // Step -1 We have to include the package in the working file
+
+public class Main {    // Step -2 We have to set the class name same as our file name at the starting of the program
+    public static void main(String[] args) {
+        System.out.println("This is the First Line");// Step-4 use to show the output in the output screen
+        System.out.println("This is the Second Line ");
+        System.out.println("This is the Third Line ");
+        System.out.println(3+3);
+        System.out.print("Hello this is line is printed with the help of the print !!");
+        System.out.print("Hello this is line is printed with the help of the print");
+
+
+    }
+}
+
---
+++ Basic_Programs/src/Programs/Variables.java
@@ -0,0 +1,26 @@
+package Programs;
+
+public class Variables {
+    public static void main(String[] args){
+        String name = "John";
+        Integer age = 22;
+        Float height = 3.145f ;
+        Double area = 3.2589;
+        System.out.println(name );
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(area);
+
+        
+// Assign the multiple variables
+        int x=10 ,y=20 , z=30;
+        System.out.println(x+y+z);
+
+
+// Assign One value to Multiple Variables
+        int a, b, c;
+        a = b = c = 50;
+        System.out.println(a + b + c);
+    }
+
+}
