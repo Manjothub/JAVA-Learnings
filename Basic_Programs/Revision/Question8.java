82043f60509951718a2969ad7737a0b8ed3ee9ef
Manjot08 <59700302+Manjothub@users.noreply.github.com>
2022-05-31 23:34:45 +0530

    Add files via upload

---
+++ Basic_Programs/Revision/Question8.java
@@ -0,0 +1,13 @@
+package Revision;
+
+
+public class Question8 {
+    public static void main(String[] args) {
+
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+
+}
---
+++ Basic_Programs/Revision/Question9.java
@@ -0,0 +1,10 @@
+package Revision;
+
+// Write a Java program to compute the specified expressions and print the output.
+//Test Data:
+//((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+public class Question9 {
+    public static void main(String[] args){
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
